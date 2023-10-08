0c06e50877b1a34115ffe736684def5f66e4ae0e
Vladimir Khaidanov <khaidanov@gmail.com>
2023-10-08 17:55:12 +0300

    +

--- all-in-one/main.clj
+++ all-in-one/main.clj
@@ -12,3 +12,15 @@ comment(
 
 (->> 1 (+ 2) (- 4)) 
 ;;(- 4 (+ 2 1))
+
+(defn inc_first [n]
+  (-> n  inc))
+(-> 1 inc inc inc)
+(println (map (fn [n]
+             (-> n inc))  #{1 2 3} ))
+
+(-> #{1 2} first inc)
+
+;;hash map
+(println (hash-map :key 1 :value 'value) :value)
+(println (get {:key 1 :value 'value} :value1 "missing"))
