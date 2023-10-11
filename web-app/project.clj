bbd0c474514bd704e30e647c0ebfdea8185d6d23
Vladimir Khaidanov <khaidanov@gmail.com>
2023-10-11 15:20:50 +0300

    +

--- web-app/project.clj
+++ web-app/project.clj
@@ -4,7 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [ring/ring-jetty-adapter "1.10.0"]]
+                 [ring/ring-jetty-adapter "1.10.0"]
+                 [hiccup "2.0.0-RC2"]]
   :main ^:skip-aot web-app.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
--- web-app/src/web_app/core.clj
+++ web-app/src/web_app/core.clj
@@ -1,12 +1,14 @@
 (ns web-app.core
   (:require [ring.adapter.jetty :as jetty])
+  (:require [hiccup2.core :as h])
+  
   (:gen-class))
 
 (defn handler
   [request]
   {:status 200
-   :headers {"Content-Type" "test/html"}
-   :body "Hello World!"   
+   :headers {"Content-Type" "text/html"}
+   :body (str(h/html [:div "Test"]))
   })
 
 (defn -main
